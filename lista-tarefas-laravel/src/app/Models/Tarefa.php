21a98c1c8997640c56be6c5270442da61bd8ad9a
Lipezin-16 <kauan.fillipe@estudante.ifgoiano.edu.br>
2026-03-09 08:25:50 -0300

    sla so quero que saia

--- lista-tarefas-laravel/src/app/Models/Tarefa.php
+++ lista-tarefas-laravel/src/app/Models/Tarefa.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tarefa extends Model
 {
     protected $fillable = ['titulo', 'descricao', 'status'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
